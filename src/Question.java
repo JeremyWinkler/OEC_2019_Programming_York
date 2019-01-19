360aa7e327eba6cbcae46ec03968f7dc23c81522
JeremyWinkler <jeremy.winkler1@gmail.com>
2019-01-18 21:13:39 -0500

    add question class

---
+++ src/Question.java
@@ -0,0 +1,11 @@
+
+public class Question {
+	public String q; // Question
+	public String a; // Answer
+//	public int answerType;
+	
+	public Question (String question, String answer) {
+		q = question;
+		a = answer;
+	}
+}
