90159ae66b83f3adbf6a7bc5b74270e01b8eab19
shyam1596 <shyam123@my.yorku.ca>
2019-01-19 00:59:11 -0500

    GUI Final

--- src/AddQuestion.java
+++ src/AddQuestion.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class AddQuestion extends JFrame {
@@ -36,7 +37,7 @@ public class AddQuestion extends JFrame {
 		mainPanel.add(new JLabel("Short Version of the Question"));
 		mainPanel.add(textField2);
 
-		String[] options = { "Yes", "No" };
+		String[] options = { "Multiple Choice", "Checklist" };
 
 		currentPattern = options[0];
 		currentSel = 0;
@@ -44,7 +45,7 @@ public class AddQuestion extends JFrame {
 		JComboBox patternList = new JComboBox(options);
 		patternList.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (currentPattern.equals("Yes"))
+				if (currentPattern.equals("Multiple Choice"))
 					choice = true;
 				else
 					choice = false;
@@ -58,6 +59,8 @@ public class AddQuestion extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				Question q = new Question(textField.getText(), textField2.getText(), choice);
 				AddAnswer ex = new AddAnswer(q);
+				textField.setText("");
+				textField.setText("");
 				questions.add(q);
 				ex.setVisible(true);
 			}
@@ -74,6 +77,14 @@ public class AddQuestion extends JFrame {
 		button2.setActionCommand("Button2");
 		button2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				FormCreator f;
+				try {
+					f = new FormCreator();
+					f.printForm(questions);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				
 			}
 		});
