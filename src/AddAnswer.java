d0f69ff59c092cdb7f960a033d02a29329faa1b8
shyam1596 <shyam123@my.yorku.ca>
2019-01-19 00:30:55 -0500

    fuck

--- src/AddAnswer.java
+++ src/AddAnswer.java
@@ -1,13 +1,15 @@
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
 public class AddAnswer extends JFrame {
 	GridLayout layout1 = new GridLayout(2, 1);
+	ArrayList<Answer> answers = new ArrayList<Answer>();
 
-	public AddAnswer() {
+	public AddAnswer(Question q) {
 		GridLayout layout = new GridLayout(1, 5);
 		JLabel label = new JLabel("What is your answer?");
 		JTextField textField = new JTextField("Write your answer here");
@@ -30,6 +32,9 @@ public class AddAnswer extends JFrame {
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Answer a = new Answer(textField.getText(), Integer.parseInt(textField2.getText()));
+				answers.add(a);
+				textField.setText("");
+				textField2.setText("");
 			}
 		});
 		panel.add(button);
@@ -40,17 +45,14 @@ public class AddAnswer extends JFrame {
 		mainPanel.setLayout(layout1);
 		mainPanel.add(panel);
 		
-		JButton button2 = new JButton("Exit");
+		JButton button2 = new JButton("Done");
 		button2.setActionCommand("Button1");
 		button2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				q.answer = answers;
+				dispose();
 			}
 });
 		mainPanel.add(button2);
-	}
-
-	public static void main(String[] args) {
-		AddAnswer ex = new AddAnswer();
-		ex.setVisible(true);
 	}
 }
--- src/AddQuestion.java
+++ src/AddQuestion.java
@@ -8,54 +8,76 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class AddQuestion extends JFrame {
 
-	GridLayout layout = new GridLayout(1, 5);
-	JLabel label = new JLabel("What is your question?");
-	JTextField textField = new JTextField("Write your answer here");
-	JTextField textField2 = new JTextField("Write your answer here");
-	String currentPattern;
-	int currentSel;
+	GridLayout layout1 = new GridLayout(2, 1);
 	boolean choice;
+	
 	public AddQuestion() {
+		GridLayout layout = new GridLayout(1, 5);
+		ArrayList<Question> questions = new ArrayList<Question>();
+		JLabel label = new JLabel("What is your question?");
+		JTextField textField = new JTextField("Write your answer here");
+		JTextField textField2 = new JTextField("Write your answer here");
+		String currentPattern;
+		int currentSel;
+	
+
 		JPanel mainPanel = new JPanel();
 		setSize(800, 1000);
 		setContentPane(mainPanel);
 		mainPanel.setLayout(layout);
-		
+
 		mainPanel.add(new JLabel("What is your question"));
 		mainPanel.add(textField);
-		
+
 		mainPanel.add(new JLabel("Short Version of the Question"));
-		mainPanel.add(textField2 );
-		 
-		 String[] options = {"Yes", "No"};
-		 
-		 currentPattern = options[0];
-		 currentSel = 0;
-		 
-		 JComboBox patternList = new JComboBox(options);
-		 patternList.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					if (currentPattern.equals("Yes"))
-						choice=true;
-					else
-						choice=false; 
-				}
-			});
-		 mainPanel.add(patternList);
-		 
-		 JButton button = new JButton("Add");
-		 button.setActionCommand("Button1");
-		 button.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					Question q = new Question(textField.getText(),textField2.getText(),choice);
-					AddAnswer ex = new AddAnswer();
-					ex.setVisible(true);
-				}
-	});
-		 mainPanel.add(button);
+		mainPanel.add(textField2);
+
+		String[] options = { "Yes", "No" };
+
+		currentPattern = options[0];
+		currentSel = 0;
+
+		JComboBox patternList = new JComboBox(options);
+		patternList.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (currentPattern.equals("Yes"))
+					choice = true;
+				else
+					choice = false;
+			}
+		});
+		mainPanel.add(patternList);
+
+		JButton button = new JButton("Add");
+		button.setActionCommand("Button1");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Question q = new Question(textField.getText(), textField2.getText(), choice);
+				AddAnswer ex = new AddAnswer(q);
+				questions.add(q);
+				ex.setVisible(true);
+			}
+		});
+		mainPanel.add(button);
+
+		JPanel panel = new JPanel();
+		setSize(800, 1000);
+		setContentPane(panel);
+		panel.setLayout(layout1);
+		panel.add(mainPanel);
+
+		JButton button2 = new JButton("Publish");
+		button2.setActionCommand("Button2");
+		button2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		panel.add(button2);
 	}
 
 	public static void main(String[] args) {
